902cdd5ac40edc3a1db60983bd49ae7975564530
ChARCOOL <bordobereli435@gmail.com>
2019-10-17 18:48:43 +0300

    Initial commit

---
+++ Ricardo.AIO/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reflection;
+using System.Security.Permissions;
+using EnsoulSharp.SDK;
+using Ricardo.AIO.Properties;
+
+namespace Ricardo.AIO
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnGameLoad;
+        }
+
+        [PermissionSet(SecurityAction.Assert, Unrestricted = true)]
+        private static void OnGameLoad()
+        {
+            LoadDll(Resources.Ricardo_AIO, "Ricardo.AIO.Program");
+        }
+
+        private static void LoadDll(byte[] dll, string namespacePlusClass)
+        {
+            try
+            {
+                var a = Assembly.Load(dll);
+                var myType = a.GetType(namespacePlusClass);
+                var methon = myType.GetMethod("Main", BindingFlags.Public | BindingFlags.Static);
+
+                if (methon != null) methon.Invoke(null, null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
